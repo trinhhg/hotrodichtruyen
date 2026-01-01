d25b21afaab9982179e34a4f73bce913fbfd3247
Trịnh Hg <onepiecechapter@gmail.com>
2026-01-01 21:13:35 +0700

    Add notification details to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+🔔 Thông báo mới
+📱 App: MB Bank
+📝 Tiêu đề: Thông báo biến động số dư
+💬 Nội dung: TK 09xxx211|GD: -102,000VND 21/12/25 16:54 |SD: 2VND|DEN: PHAM TRUNG ANH DUNG - 9785013456|ND: TRINH THI XUAN HUONG chuyen tien
+⏰ Thời gian: 09:54:50 21/12/2025
